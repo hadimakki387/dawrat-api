9257642e59e47f9799df0a26071ddfe12ce68882
hadimakki387 <hmakki387@gmail.com>
2023-12-11 19:24:01 +0200

    debugging

--- src/app/api/documents/route.ts
+++ src/app/api/documents/route.ts
@@ -4,19 +4,19 @@ import httpStatus from "http-status";
 import { createDocument } from "@/backend/modules/Documents/document.service";
 
 export async function DELETE(req: NextRequest) {
-  const body = await req.json();
-  const deleteFiles = await utapi.deleteFiles(body.doc);
-  console.log(deleteFiles);
+  // const body = await req.json();
+  // const deleteFiles = await utapi.deleteFiles(body.doc);
+  // console.log(deleteFiles);
 
-  if (!deleteFiles.success) {
-    return new NextResponse(
-      JSON.stringify({ message: "Somthing Went Wrong" }),
-      { status: httpStatus.INTERNAL_SERVER_ERROR }
-    );
-  }
+  // if (!deleteFiles.success) {
+  //   return new NextResponse(
+  //     JSON.stringify({ message: "Somthing Went Wrong" }),
+  //     { status: httpStatus.INTERNAL_SERVER_ERROR }
+  //   );
+  // }
 
   return new NextResponse(JSON.stringify({ message: "Deleted Successfully" }), {
-    status: httpStatus.OK,
+    status: httpStatus.INTERNAL_SERVER_ERROR,
   });
 }
 
