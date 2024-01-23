b167905759e3a18b4528232ea0b8e24ed4c0e552
hadimakki387 <hmakki387@gmail.com>
2024-01-23 20:33:11 +0200

    fix

--- src/app/api/languages/route.ts
+++ src/app/api/languages/route.ts
@@ -1,8 +1,10 @@
 import { returnArrayData } from "@/backend/helper-functions/returnData";
 import { Language } from "@/backend/modules/Documents/document.model";
+import MongoConnection from "@/backend/utils/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    MongoConnection()
     const languages = await Language.find({})
     return NextResponse.json(returnArrayData(languages));
 }
